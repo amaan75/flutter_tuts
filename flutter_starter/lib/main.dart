7636d69dc2347379078fe98f4d6b34c5eb0f1e34
Aman Gulati <coolamangulati75@gmail.com>
2018-04-19 22:14:44 +0530

    sample flutter project and new hello flutter project

---
+++ flutter_starter/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    new Center(
+      child: new Text(
+        'Hello, world!',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
