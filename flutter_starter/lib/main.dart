7a66b8abbb99f46004033c4149461304d5a6207e
Aman Gulati <coolamangulati75@gmail.com>
2018-04-19 22:26:57 +0530

    added a button

--- flutter_starter/lib/main.dart
+++ flutter_starter/lib/main.dart
@@ -3,10 +3,27 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     new Center(
-      child: new Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
-      ),
+      child: new MyApp(),
     ),
   );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Hello Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Hello Flutter'),
+        ),
+        body: new Center(
+          child: new RaisedButton(
+            onPressed: null,
+            child: new Text('My First Button'),
+          ),
+        ),
+      ),
+    );
+  }
 }
