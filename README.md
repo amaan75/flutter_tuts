401934f8992de97d3af0cf0e5f479a6d4363990e
Aman Gulati <coolamangulati75@gmail.com>
2018-04-20 09:59:27 +0530

    completed flutter2

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
     
 1. helloworld
 2. [flutter_starter](https://github.com/amaan75/flutter_tuts/tree/master/flutter_starter)
+3. [flutter2](https://github.com/amaan75/flutter_tuts/tree/master/flutter2)
--- flutter2/lib/main.dart
+++ flutter2/lib/main.dart
@@ -5,6 +5,61 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp();
+    return new MaterialApp(
+      title: 'Basic Layouts',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Basic Layouts'),
+        ),
+        body: new Container(
+          padding: const EdgeInsets.all(32.0),
+          child: new Center(
+            child: new Column(
+              children: <Widget>[
+                new Text('Hello'),
+                new Text('World'),
+                new Text('!!!'),
+                new Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    new Container(
+                      padding: const EdgeInsets.all(4.0),
+                      child: new Column(
+                        children: <Widget>[
+                          new Text('Hello'),
+                          new Text('World'),
+                          new Text('!!!'),
+                        ],
+                      ),
+                    ),
+                    new Container(
+                      padding: const EdgeInsets.all(4.0),
+                      child: new Column(
+                        children: <Widget>[
+                          new Text('Hello'),
+                          new Text('World'),
+                          new Text('!!!'),
+                        ],
+                      ),
+                    ),
+                    new Container(
+                      padding: const EdgeInsets.all(4.0),
+                      child: new Column(
+                        children: <Widget>[
+                          new Text('Hello'),
+                          new Text('World'),
+                          new Text('!!!'),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
