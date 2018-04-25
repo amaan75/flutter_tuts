6d5eec1f0b3b49ff02d409aa1966ef73232fc21f
Amaan <amangulati075@gmail.com>
2018-04-25 11:26:55 +0530

    initial commit for text field demo

---
+++ text_field_demo/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MaterialApp(home: new MyApp()));
+
+class MyApp extends StatefulWidget {
+  MyAppState createState() => new MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
